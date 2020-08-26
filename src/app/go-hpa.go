c6b407a89164a72458ef70220b2f8650088b4f1f
farjax <farjax@gmail.com>
2020-08-26 19:02:59 -0300

    correcao do hpa para apps/v1

--- src/app/go-hpa.go
+++ src/app/go-hpa.go
@@ -3,15 +3,23 @@ package main
 import(
 	"math"
 	"fmt"
+  "log"
+  "net/http"
 )
-func main(){
-	calcular_raiz()
-  fmt.Printf("Code.education Rocks!!")
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  calcular_raiz()
+  fmt.Fprintf(w, "Code.education Rocks!")
+}
+
+func main() {  
+  http.HandleFunc("/", handler)
+  log.Fatal(http.ListenAndServe(":8083", nil))
 }
 
 func calcular_raiz() float64{
   var x float64 =0.0001
-  for i:=1;i<100000000;i++ {
+  for i:=1;i<1000000;i++ {
     x+= math.Sqrt(x)
   }
   return x
